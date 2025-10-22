23026eeb721c1416f097d4653fd82b0e9a3e721d
Brandon <bbeuttel@gmail.com>
2025-10-22 12:08:06 -0700

    psudocoded useAuth

--- client/src/auth/useAuth.ts
+++ client/src/auth/useAuth.ts
@@ -1,10 +1,13 @@
 import { useContext } from 'react';
 import { AuthContext } from './authContext';
 
+//react hook that defines the context provided from AuthProvider
 const useAuth = () => {
-    const ctx = useContext(AuthContext);
-    if(!ctx) throw new Error('useAuth must be used within <AuthProvider>');
-    return ctx
+    //defining the context using useContextHook
+    const context = useContext(AuthContext);
+    //if no context throw error
+    if(!context) throw new Error('useAuth must be used within <AuthProvider>');
+    return context;
 }
 
 export default useAuth;
