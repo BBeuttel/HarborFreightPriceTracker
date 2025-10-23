dfa5049c5a3c23fe6b99b84d9cf9bed12e813a56
Brandon <bbeuttel@gmail.com>
2025-10-23 11:28:50 -0700

    fully psudocoded all code up tot his point

--- client/src/lib/auth.ts
+++ client/src/lib/auth.ts
@@ -1,23 +1,27 @@
 import supabase from "./supabase";
 import type { User, Session } from '@supabase/supabase-js';
 
+// type here that is defining the user and the session (signed in, signed out)
 type AuthData = {user: User | null; session: Session | null}
 
+//asynchronous helper function that returns data part of type AuthData (line 5) using signUp method from supabase.  if email and password do not fit criteria of email or PW (this is not sign in so they cant be incorrect), will throw error
 const signUpUser = async (email: string, password: string): Promise<AuthData> => {
     const { data, error } = await supabase.auth.signUp({ email, password });
     if (error) throw error;
     return data;
 }
 
+//asynchorous helper function that returns data of type AuthData (line 5) when a user tries to sign in using the signInWithPassword method built into supabase.  if email and password are incorrect will throw error
 const signInUser = async (email: string, password: string): Promise<AuthData> => {
     const { data, error } = await supabase.auth.signInWithPassword({ email, password});
     if (error) throw error;
     return data
 }
 
+//asynchonous helper function that invalites current session (signs out).  AuthProvider will see onAuthStateChange and update context to null for both user and session
 const signOutUser = async (): Promise<void> => {
     const { error } = await supabase.auth.signOut();
     if (error) throw error;
 }
 
-export default { signUpUser, signInUser, signOutUser };
+export { signUpUser, signInUser, signOutUser };
--- client/src/lib/authState.ts
+++ client/src/lib/authState.ts
@@ -1,12 +1,18 @@
 import supabase from './supabase';
 import type { Session } from '@supabase/supabase-js'; 
 
-
+// Function that takes in a callback function (cb) whose argument is the current session
 const onAuthChange = (cb: (session: Session | null) => void): (() => void) => {
+    //varible that is the destructured subscription object when OnAuthStateChange is called.
+    //if there is a change in the state of the session the onAuthStateChange method runs and triggers the callback function to run again with the updates session state.
     const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => cb(session))
+    //return a fucntion that calls the unsubsbribe method telling supabase to stop listening for state chnages
     return () => subscription.unsubscribe()
 }
 
+// asynchornous function that gets the current state of the session (signed in, signed out) from Supabase
+// supabase stores session locally after user logs in
+// if user not signed in data.session will be null
 const getSession = async () => {
     const { data } = await supabase.auth.getSession()
     return data.session
